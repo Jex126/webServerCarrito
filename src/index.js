c59fc29082d86acb5f73b86a62e4f6378670f015
Neftali Ceron <jeresergor@gmail.com>
2024-11-05 12:02:28 -0600

    impresion por consola del puerto de escucha

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import route  from "./routes/statusCarrito.route.js";
 const app = express();
-app.listen(80);
+app.listen(80,()=>{
+    console.log("Servidor escuchando en el puerto: 80")
+});
 app.disable('x-powered-by');
 app.use(route);
 
